82efd7dee57b8d62ebe897d5aa99161e6697d335
Fuad <fujam20@student.sdu.dk>
2021-12-28 14:18:01 +0100

    add project

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 80
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
