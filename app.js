b9f6be735604d4606baab19ff514538f27bda409
Fuad <fujam20@student.sdu.dk>
2022-01-08 22:37:46 +0100

    added shopping cart

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const fetch = require('cross-fetch');
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const backend_url = 'https://mobilny-backend.herokuapp.com'
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.status(404).send("Sorry this domain isn't ready yet!")
+  //res.status(404).send("Sorry this domain isn't ready yet!")
 
-  /*
+  
   (async () => {
     try {
       let data = await fetch(backend_url+'/phones');
@@ -25,13 +25,13 @@ app.get('/', (req, res) => {
       console.error(err);
     }
   })();
-  */
+  
 })
 
 
 app.get('/:phone/models', (req, res) => {
-  res.status(404).send("Sorry this domain isn't ready yet!")
-  /*
+  //res.status(404).send("Sorry this domain isn't ready yet!")
+  
   (async () => {
     try {
       let data = await fetch(backend_url+'/phones/'+req.params.phone + '/models');
@@ -41,19 +41,27 @@ app.get('/:phone/models', (req, res) => {
       }
       
       let models = await data.json();
-    
-      res.render('Vaelg-Model',{models: models })
+      
+
+      if(models.length == 0){
+        res.status(404).send("phone not found")
+      }
+      else{
+        res.render('Vaelg-Model',{models: models })
+
+      }
+      
     } catch (err) {
       console.error(err);
     }
   })();
-  */
+  
 })
 
 
 app.get('/:phone/models/:model', (req, res) => {
-  res.status(404).send("Sorry this domain isn't ready yet!")
-  /*
+  //res.status(404).send("Sorry this domain isn't ready yet!")
+  
   (async () => {
     try {
       let data = await fetch(backend_url+'/phones/'+req.params.phone+ '/models/'+ req.params.model);
@@ -64,6 +72,11 @@ app.get('/:phone/models/:model', (req, res) => {
 
       
       let model = await data.json();
+      console.log(model)
+
+      if(model.parts.length == 0){
+        res.status(404).send("Model not found")
+      }
 
     
       res.render('Produkt',{model: model })
@@ -71,20 +84,19 @@ app.get('/:phone/models/:model', (req, res) => {
       console.error(err);
     }
   })();
-  */
+  
 })
 
 
 app.get('/kontakt', (req, res) => {
-  res.status(404).send("Sorry this domain isn't ready yet!")
+  //res.status(404).send("Sorry this domain isn't ready yet!")
 
-  //res.render('Kontakt')
+  res.render('Kontakt')
 })
 
 app.get('/om', (req, res) => {
-  res.statusCode = 404
-  res.send('Site is not ready')
-  //res.render('om')
+  //res.status(404).send("Sorry this domain isn't ready yet!")
+  res.render('om')
 })
 
 
